46655a11baa5f3141521695c7af7aa25d0573665
Alhanouf Allazzam <115586263+AlhanoufKMLz@users.noreply.github.com>
2026-02-15 20:15:53 +0300

    task manager home page + task details page

---
+++ src/app/services/task-service.ts
@@ -0,0 +1,37 @@
+import { Injectable, signal } from '@angular/core';
+
+export interface Task {
+  id: number;
+  title: string;
+  description: string;
+  completed: boolean;
+  createdAt: Date;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TaskService {
+  private tasksSignal = signal<Task[]>([
+    {
+      id: 1,
+      title: 'Learn Angular Basics',
+      description: 'Understand components, services, and routing',
+      completed: true,
+      createdAt: new Date('2026-01-02'),
+    },
+    {
+      id: 2,
+      title: 'Build a Project',
+      description: 'Create a task manager application',
+      completed: false,
+      createdAt: new Date('2026-02-15'),
+    }
+  ]);
+
+  tasks = this.tasksSignal.asReadonly();
+
+  getTask(id: number){
+    return this.tasks().find(task => task.id === id)
+  }
+}
