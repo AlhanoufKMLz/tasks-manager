cb1946d9ba190959fea8b158927ffece69c0e172
Alhanouf Allazzam <115586263+AlhanoufKMLz@users.noreply.github.com>
2026-02-16 19:25:55 +0300

    sort tasks

--- src/app/pages/task-list/task-list.ts
+++ src/app/pages/task-list/task-list.ts
@@ -12,6 +12,7 @@ import { CommonModule } from '@angular/common';
 export class TaskList {
   taskService = inject(TaskService);
   filter =  signal<'all' | 'completed' | 'active'>('all');
+  sort =  signal<'creationDay' | 'dueDate'>('creationDay');
   filteredTasks = computed(()=> {
     switch(this.filter()) {
       case 'all':
@@ -29,8 +30,54 @@ export class TaskList {
     this.filter.set(filter);
   }
 
+  setSort(sort: 'creationDay' | 'dueDate'){
+    this.sort.set(sort);
+    this.sortBy();
+  }
+
   deleteTask(id: number){
     this.taskService.deleteTask(id);
+  }
+
+  sortBy(){
+    if(this.sort() == 'creationDay'){
+      this.filteredTasks().sort((a, b) => { 
+        const aCreated = new Date(a.createdAt).getTime();
+        const bCreated = new Date(b.createdAt).getTime();
+  
+        // both completed
+        if (a.completed && b.completed) return aCreated - bCreated;
+  
+        // one is complete
+        if (a.completed ) return 1;
+        if (b.completed) return -1;
+  
+        // both not completed
+        return aCreated - bCreated;
+      });
+    }
+
+    if (this.sort() === 'dueDate') {
+      this.filteredTasks().sort((a, b) => {
+        const today = new Date().getTime();
+  
+        const aDue = new Date(a.dueDate).getTime();
+        const bDue = new Date(b.dueDate).getTime();
+  
+        const aExpired = aDue < today;
+        const bExpired = bDue < today;
+  
+        // both expired
+        if (aExpired && bExpired) return aDue - bDue;
+  
+        // one is expired
+        if (aExpired) return 1;
+        if (bExpired) return -1;
+  
+        // both not expired
+        return aDue - bDue;
+      });
+    }
   }
 
 }
